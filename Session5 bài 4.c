43dc9b2a5800b708cd8cfc5abb3e8f362079748d
Megood1210 <xuanvinhle06@gmail.com>
2024-11-25 11:54:29 +0700

    Upload bài tập 4

---
+++ Session5 bài 4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+
+	int i=2
+    for(i=2;i<=9;i++) {
+        for (int j=2; j<=9; j++) {
+            printf("%d x %d = %2d ", j, i, j*i);
+        }
+        printf("\n");
+    }
+    return 0;
+
+    
+
+    return 0;
+}
+
